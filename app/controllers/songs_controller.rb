9a996d35fc09b6fdfa671a7cf6eb36147787748e
joshua <joshuamtaylor455@gmail.com>
2023-05-08 15:22:49 -0400

    fix errors in create action, add show action and route

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,16 +6,22 @@ class SongsController < ApplicationController
 
   def create
     @songs = Song.create(
-      name: params[:name]
-      artist: params[:artist]
-      year: params[:year]
-      genre: params[:genre]
-      tempo: params[:tempo]
-      time: params[:time]
-      dynamics: params[:dynamics]
-      song_length: params[:song_length]
-      video_url: params[:video_url]
-      sheet_music_url: params[:sheet_music_url]
+      name: params[:name],
+      artist: params[:artist],
+      year: params[:year],
+      genre: params[:genre],
+      tempo: params[:tempo],
+      time: params[:time],
+      dynamics: params[:dynamics],
+      song_length: params[:song_length],
+      video_url: params[:video_url],
+      sheet_music_url: params[:sheet_music_url],
     )
     render :show
+  end
+
+  def show
+    @song = Song.find_by(id: params[:id])
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "/songs" => "songs#index"
 
+  get "/songs/:id" => "songs#show"
+
   post "/songs" => "song#create"
 end
