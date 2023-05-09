12d817e38e775b4ec909f0ab7441fe7fc358235d
joshua <joshuamtaylor455@gmail.com>
2023-05-09 13:04:15 -0400

    adds route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  #song routes
   get "/songs" => "songs#index"
-
   get "/songs/:id" => "songs#show"
-
   post "/songs" => "songs#create"
-
   patch "songs/:id" => "songs#update"
-
   delete "songs/:id" => "songs#destroy"
+
+  #user routes
+  post "/users" => "users#create"
 end
